02940b14102862285a2e8e39afd85238843913be
Alex Thoennes <Alex@Alexanders-MacBook-Pro.local>
2016-09-24 13:09:47 -0400

    Node Class
    This is used every time a node is inserted.

---
+++ TreeNode.java
@@ -0,0 +1,17 @@
+public class TreeNode 
+{
+	public int value;
+	
+	TreeNode left;
+	
+	TreeNode right;
+	
+	public TreeNode(int value)
+	{
+		this.value = value;
+		
+		this.left = null;
+		
+		this.right = null;
+	}
+}
